29c16d9f02a4d103c22b4bc2b5bae118183f0019
Aayushy7777 <aayu.sh7021@gmail.com>
2026-03-28 13:26:20 +0530

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,12 +1,5 @@
-// const asyncHandler = () => {
-
-
- 
-//     }
-
-
 const asyncHandler = (requestHandler) => {
-        (req, res, next) => {
+       return (req, res, next) => {
             Promise.resolve(requestHandler(req, res, next))
             .catch((err) => {
                 res.status(err.code || 500).json({
